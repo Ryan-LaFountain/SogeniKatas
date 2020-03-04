81908ca44e06e423172c425a02dfc5177a89e757
rlafounta <rlafounta@saionline.com>
2020-03-03 22:15:14 -0600

    Added Feature 1 for Fizz Buzz

---
+++ SogeniKatas/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SogeniKatas
+{
+    public static class FizzBuzz
+    {
+        public static string EvaluateNumber(int number)
+        {
+            if (IsFizzBuzz(number))
+                return "fizz buzz";
+            else if (IsFizz(number))
+                return "fizz";
+            else if (IsBuzz(number))
+                return "buzz";
+            else
+                return number.ToString();
+        }
+
+        public static bool IsFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        public static bool IsBuzz(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        public static bool IsFizzBuzz(int number)
+        {
+            return IsFizz(number) && IsBuzz(number);
+        }
+    }
+}
--- SogeniKatas/Program.cs
+++ SogeniKatas/Program.cs
@@ -6,8 +6,34 @@ namespace SogeniKatas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool active = true;
+            Console.WriteLine("Welcome to FizzBuzz!");
+            while (active)
+            {
+                Console.WriteLine("Enter a number.");
+                string input = Console.ReadLine();
+                int number;
+                if (input == "Exit")
+                {
+                    active = false;
+                    Console.WriteLine("Exiting...");
+                    Console.WriteLine("Thank you for playing Fizz Buzz!");
+                    Console.ReadLine();
+                    continue;
+                }
 
+                if(!Int32.TryParse(input, out number))
+                {
+                    Console.WriteLine($"Error: Unable to Convert {input} to a number.");
+                    Console.WriteLine("Enter a number.");
+                    Console.ReadLine();
+                }
+
+                string output = FizzBuzz.EvaluateNumber(number);
+
+                Console.WriteLine(output);
+            }
+            
         }
     }
 }
